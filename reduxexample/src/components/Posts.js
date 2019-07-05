8e201a846b3cd058da6a1365144e5a521249f76f
Vita <vitalia@myrambler.ru>
2019-07-05 13:01:00 +0300

    + redux chrome extension, + propTypes

--- reduxexample/src/components/Posts.js
+++ reduxexample/src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import PropTypes from "prop-types"
 import { connect } from "react-redux";
 import {fetchPosts} from "../actions/postActions"
 
@@ -20,6 +21,11 @@ import {fetchPosts} from "../actions/postActions"
 			</div>
 			)
 	}
+}
+
+Posts.propTypes = {
+	fetchPosts: PropTypes.func.isRequired,
+	posts: PropTypes.array.isRequired
 }
 
 const mapStateToProps = state =>({
