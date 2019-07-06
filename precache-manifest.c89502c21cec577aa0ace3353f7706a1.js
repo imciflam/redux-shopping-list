8b073822f850ef3ac98d1dc46a876ac1b45fd086
Vita <vitalia@myrambler.ru>
2019-07-06 11:44:25 +0300

    Updates

---
+++ precache-manifest.c89502c21cec577aa0ace3353f7706a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573d7c32315a4143e11158372c388dbb",
+    "url": "/imciflam/redux-shopping-list/index.html"
+  },
+  {
+    "revision": "422a53d52373ae63e04e",
+    "url": "/imciflam/redux-shopping-list/static/css/main.441c33cf.chunk.css"
+  },
+  {
+    "revision": "2004e9dcd0fb1a2ef828",
+    "url": "/imciflam/redux-shopping-list/static/js/2.6e9c4761.chunk.js"
+  },
+  {
+    "revision": "422a53d52373ae63e04e",
+    "url": "/imciflam/redux-shopping-list/static/js/main.3f8b1726.chunk.js"
+  },
+  {
+    "revision": "98aed291c7ee22c09a9c",
+    "url": "/imciflam/redux-shopping-list/static/js/runtime~main.6a0c0d71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/imciflam/redux-shopping-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/imciflam/redux-shopping-list/precache-manifest.c89502c21cec577aa0ace3353f7706a1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/imciflam/redux-shopping-list/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
