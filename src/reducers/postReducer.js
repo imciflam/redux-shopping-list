01f6083bc3b867ed20984ea76c6416af34dd2811
Vita <vitalia@myrambler.ru>
2019-07-06 11:33:13 +0300

    linting

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,25 @@
+//evaluation of all actions going on here (type is mandatory bc of it)
+import { FETCH_POSTS, NEW_POST } from "../actions/types";
+
+const initialState = {
+  items: [], //posts from action where there's a fetch req
+  item: {} //single post
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+      return {
+        ...state,
+        items: action.payload
+      };
+    case NEW_POST:
+      return {
+        //return only one new item
+        ...state,
+        item: action.payload //single post
+      };
+    default:
+      return state;
+  }
+}
